15cb8325efc8d8e5b14e2d0316a71cc1c233e703
Dzhansu Zalim <djansuevil@gmail.com>
2024-10-12 15:20:36 +0300

    Your commit message

---
+++ petstagram/common/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from petstagram.common.models import Comment, Like
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'date_time_of_publication', 'to_photo')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('to_photo', )
